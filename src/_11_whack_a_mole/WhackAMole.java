d0193c38b22bee7397448b50e487f0b5146baf15
root <root@6ae29f4c4a18>
2021-10-24 19:48:17 +0000

    10/24/21

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,17 +1,43 @@
 package _11_whack_a_mole;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
+import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	Random ran = new Random();
+	int ranNum;
 	
 	void run() {
-		
+		ranNum = ran.nextInt(24);
+		frame.setVisible(true);
+		frame.setSize(200, 200);
+		drawButtons(ranNum);
+		frame.add(panel);
+	}
+	
+	void drawButtons(int ran) {
+		for (int i = 0; i < 24; i++) {
+			if (i == ran) {
+				JButton mole = new JButton();
+				mole.setText("mole!");
+				panel.add(mole);
+				mole.addActionListener(this);
+			}
+			else {
+				JButton buttoni = new JButton();
+				panel.add(buttoni);
+				buttoni.addActionListener(this);
+			}
+		}
 	}
 	
 	static void speak(String words) {
@@ -37,5 +63,13 @@ public class WhackAMole {
 	    JOptionPane.showMessageDialog(null, "Your whack rate is "
 	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
 	                  + " moles per second.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(mole)) {
+			
+		}
+		
 	}
 }
