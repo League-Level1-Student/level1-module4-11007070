e455eb9214a3419fbd3b91608390dd387122041f
root <root@bd8354a7ed24>
2021-12-19 20:45:35 +0000

    12/19/21

--- src/Level1PracticeTestCode.java
+++ src/Level1PracticeTestCode.java
@@ -4,24 +4,29 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Level1PracticeTestCode implements ActionListener {
 	JFrame frame = new JFrame();
 	JLabel label = new JLabel();
-	JButton submitB = new JButton();
-	JButton hintB = new JButton();
-	JTextField textF = new JTextField();
+	JPanel panel = new JPanel();
+	JButton submitB = new JButton("Submit");
+	JButton hintB = new JButton("Hint");
+	JTextField textF = new JTextField(10);
+	String answer = "weights";
 	
 	void run() {
 		frame.setVisible(true);
-		submitB.setText("Submit");
-		hintB.setText("Hint");
+		//submitB.setText("Submit");
+		//hintB.setText("Hint");
 		label.setText("What heavy seven letter word can you take two away from and be left with eight?");
-		label.add(submitB);
-		label.add(hintB);
-		label.add(textF);
-		frame.add(label);
+		panel.add(label);
+		panel.add(submitB);
+		panel.add(hintB);
+		panel.add(textF);
+		frame.add(panel);
 		submitB.addActionListener(this);
 		hintB.addActionListener(this);
 		frame.pack();
@@ -30,6 +35,16 @@ public class Level1PracticeTestCode implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		if (arg0.getSource().equals(hintB)) {
+			JOptionPane.showMessageDialog(null, "Hint: You use this item in the gym.");
+		}
+		else if (arg0.getSource().equals(submitB)) {
+			if (textF.getText().equalsIgnoreCase(answer)) {
+				JOptionPane.showMessageDialog(null, "Correct Answer!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Wrong! Guess again!");
+			}
+		}
 	}
 }
